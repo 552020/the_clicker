2a62888615ed24dae1059fb5f61b54d5bf871baf
552020 <stefanolombardo@posteo.de>
2025-06-25 15:54:49 +0200

    feat: add Ethereum CSS dynamic logo

---
+++ packages/nextjs/components/EthereumCssLogo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./EthereumCssLogo.module.css";
+
+export const EthereumCssLogo = () => (
+  <div className={styles.space}>
+    <div className={styles.elogo}>
+      <div className={styles.trif + " " + styles.u1}></div>
+      <div className={styles.trif + " " + styles.u2}></div>
+      <div className={styles.trif + " " + styles.u3}></div>
+      <div className={styles.trif + " " + styles.u4}></div>
+      <div className={styles.ct}></div>
+      <div className={styles.trif + " " + styles.l1}></div>
+      <div className={styles.trif + " " + styles.l4}></div>
+    </div>
+  </div>
+);
+
+export default EthereumCssLogo;
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { hardhat } from "viem/chains";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick, useTargetNetwork } from "~~/hooks/scaffold-eth";
 
@@ -19,12 +19,17 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Home",
     href: "/",
+    icon: <HomeIcon className="h-4 w-4" title="Home" />,
   },
-
   {
     label: "Debug Contracts",
     href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
+    icon: <BugAntIcon className="h-4 w-4" title="Debug Contracts" />,
+  },
+  {
+    label: "Explorer",
+    href: "/blockexplorer",
+    icon: <MagnifyingGlassIcon className="h-4 w-4" title="Explorer" />,
   },
 ];
 
@@ -43,9 +48,9 @@ export const HeaderMenuLinks = () => {
               className={`${
                 isActive ? "bg-secondary shadow-md" : ""
               } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+              title={label}
             >
               {icon}
-              <span>{label}</span>
             </Link>
           </li>
         );
@@ -84,11 +89,12 @@ export const Header = () => {
         </details>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            {/* <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" /> */}
+            <Image alt="TheClicker logo" className="cursor-pointer" fill src="/eth-diamond-rainbow.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">TheClicker</span>
+            <span className="text-xs">Click up your life!</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
